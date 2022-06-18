d35ed7de70034d34ae6d727533f21e0ef4c16f12
Bowen Zhang <b349zhan@Bowens-MacBook-Air.local>
2022-06-18 12:58:36 -0400

    finished general navigation

--- app/src/main/java/com/example/ofn/MainActivity.kt
+++ app/src/main/java/com/example/ofn/MainActivity.kt
@@ -3,41 +3,98 @@ package com.example.ofn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.ofn.ui.theme.OFNTheme
+import com.example.ofn.Screen
+import com.example.ofn.dashboard.DashboardScreen
+import com.example.ofn.inventory.InventoryScreen
+import com.example.ofn.platform.PlatformScreen
+import com.example.ofn.settings.SettingsScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             OFNTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainApplication()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainApplication(){
+    val screens = listOf<Screen>(
+        Screen.Dashboard,
+        Screen.Inventory,
+        Screen.Platform,
+        Screen.Settings
+    )
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            BottomNavigation {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+                screens.forEach { screen ->
+                    BottomNavigationItem(
+                        icon = {
+                                Icon(
+                                    painterResource(id = screen.icon),
+                                    contentDescription = screen.title,
+                                    modifier = Modifier.size(25.dp)
+                                )
+                               },
+                        label = { Text(screen.title) },
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                // Pop up to the start destination of the graph to
+                                // avoid building up a large stack of destinations
+                                // on the back stack as users select items
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                // Avoid multiple copies of the same destination when
+                                // reselecting the same item
+                                launchSingleTop = true
+                                // Restore state when reselecting a previously selected item
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHost(navController, startDestination = Screen.Dashboard.route, Modifier.padding(innerPadding)) {
+            composable(Screen.Dashboard.route) { DashboardScreen(navController) }
+            composable(Screen.Inventory.route) { InventoryScreen(navController) }
+            composable(Screen.Platform.route) { PlatformScreen(navController) }
+            composable(Screen.Settings.route) { SettingsScreen(navController) }
+        }
+    }
 }
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     OFNTheme {
-        Greeting("Android")
+        MainApplication()
     }
 }
---
+++ app/src/main/java/com/example/ofn/Screens.kt
@@ -0,0 +1,8 @@
+package com.example.ofn
+
+sealed class Screen(var title:String, var icon:Int, var route:String){
+    object Dashboard : Screen("Dashboard", R.drawable.home,"dashboard")
+    object Inventory: Screen("Inventory",R.drawable.bag,"inventory")
+    object Platform: Screen("Platform",R.drawable.coin,"platform")
+    object Settings: Screen("Settings",R.drawable.settings,"settings")
+}
